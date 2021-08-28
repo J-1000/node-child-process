cc45c014da7a63c7a55cd4030da76b949545eadc
Jan Redmann <janredmann1@gmail.com>
2021-08-28 17:56:23 +0200

    add promise route

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express')
 const app = express()
 
-app.get('/default', (req, res) => {
-	const sum = expensiveCalculation()
-	res.send({ sum })
-})
-
-app.get('/promise', (req, res, next) => { })
-
-app.get('/child-process', (req, res) => { })
-
-app.listen(3000, () => console.log('Server listening on port 3000'))
-
-function expensiveCalculation() {
+const expensiveCalculation = () => {
 	let sum = 0
 	for (let i = 0; i < 1e9; i++) {
 		sum += i
 	}
 	return sum
 }
+
+const expensiveCalculationPromise = () =>
+	new Promise((resolve, reject) =>
+		resolve(expensiveCalculation()))
+
+app.get('/default', (req, res) => {
+	const sum = expensiveCalculation()
+	res.send({ sum })
+})
+
+app.get('/promise', async (req, res, next) => {
+	const sum = await expensiveCalculationPromise()
+	res.send({ sum })
+})
+
+app.get('/child-process', (req, res) => { })
+
+app.listen(3000, () => console.log('Server listening on port 3000'))
+
