7e74fddcf696478dec91597213bc96f74303cff8
Jan Redmann <janredmann1@gmail.com>
2021-08-28 17:40:55 +0200

    add default route

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get('/default', (req, res) => { });
+app.get('/default', (req, res) => {
+	const sum = expensiveCalculation()
+	res.send({ sum })
+})
 
-app.get('/promise', (req, res, next) => { });
+app.get('/promise', (req, res, next) => { })
 
-app.get('/child-process', (req, res) => { });
+app.get('/child-process', (req, res) => { })
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(3000, () => console.log('Server listening on port 3000'))
+
+function expensiveCalculation() {
+	let sum = 0
+	for (let i = 0; i < 1e9; i++) {
+		sum += i
+	}
+	return sum
+}
