a1be607d2be30dea7b8ae1ca92d9aa8814e52ae8
Jan Redmann <janredmann1@gmail.com>
2021-08-28 16:44:34 +0200

    add routes

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-//
+const express = require('express');
+const app = express();
+
+app.get('/default', (req, res) => { });
+
+app.get('/promise', (req, res, next) => { });
+
+app.get('/child-process', (req, res) => { });
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
